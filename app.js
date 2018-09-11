72fd391c6670dc7b3250dbb7e7a7f4a63aa3b436
CarlosCabreraGentile <carloscgentile@gmail.com>
2018-09-11 15:09:10 -0300

    connect db, register user

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const config = require('./config/database');
+
+//Connect to Database
+mongoose.connect(config.database, { useNewUrlParser: true });
+
+//Conection Message
+mongoose.connection.on('connected', () => {
+    console.log('Connected to database ' + config.database);
+});
+
+mongoose.connection.on('error', (err) => {
+    console.error('Database error ' + err);
+});
+
+//Initialize variable with express
+const app = express();
+
+const users = require('./routes/users');
+
+//Port number
+const port = 3000;
+
+//CORS Middleware
+app.use(cors());
+
+//Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Body Parser Middleware, grab the content of a request, a form, etc
+app.use(bodyParser.json());
+
+app.use('/users', users);
+
+//Index Route
+app.get('/', (req, res) => {
+    res.send('Invalid endpoint');
+});
+
+//Start Server
+app.listen(port, () => {
+    console.log("Server stated on port " + port);
+    
+});
