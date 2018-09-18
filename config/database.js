c013f43753c06fbf85f9565ce624ee862a076d1f
CarlosCabreraGentile <carloscgentile@gmail.com>
2018-09-18 11:59:21 -0300

    set mongodb

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-    database: 'mongodb://localhost:27017/meanauth',
+    // database: 'mongodb://localhost:27017/meanauth', local
+    database: 'mongodb://system011:system011@ds161312.mlab.com:61312/meanauth', //deployed
     secret: 'yoursecret'
 }
